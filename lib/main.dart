f1b945ebb98cf0a03e39a56a579a6557995d25db
magdaniele <magdaniele@uninorte.edu.co>
2022-05-31 01:40:52 -0500

    Feat: :sparkles: Adding details view of advices

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,28 @@ import 'package:diabetes_assistant/pages/login.dart';
 import 'package:diabetes_assistant/themes.dart';
 import 'package:diabetes_assistant/utils/userPreferences.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  var initializationSettingsIOS = IOSInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+      onDidReceiveLocalNotification:
+          (int? id, String? title, String? body, String? payload) async {});
+  var initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String? payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+  });
   runApp(MyApp());
 }
 
@@ -21,13 +41,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = userPreferences.myUser;
     return ThemeProvider(
-      initTheme: user.isDarkMode ? MyThemes.darkTheme : MyThemes.lightTheme,
+        initTheme: user.isDarkMode ? MyThemes.darkTheme : MyThemes.lightTheme,
         child: Builder(
             builder: (context) => MaterialApp(
-              debugShowCheckedModeBanner: false,
-                  theme: user.isDarkMode ? MyThemes.darkTheme : MyThemes.lightTheme,
+                  debugShowCheckedModeBanner: false,
+                  theme: user.isDarkMode
+                      ? MyThemes.darkTheme
+                      : MyThemes.lightTheme,
                   title: 'Login Demo',
-                  home:Login(),
+                  home: Login(),
                   scrollBehavior: ScrollBehavior(),
                 )));
   }
--- lib/privatePages/home.dart
+++ lib/privatePages/home.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors, use_full_hex_values_for_flutter_colors
+// ignore_for_file: prefer_const_constructors, use_full_hex_values_for_flutter_colors, unused_import, import_of_legacy_library_into_null_safe, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
 
 import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+import 'package:diabetes_assistant/privatePages/detail.dart';
 import 'package:diabetes_assistant/themes.dart';
 import 'package:diabetes_assistant/utils/advices.dart';
 import 'package:diabetes_assistant/widget/navigationDrawer.dart';
@@ -28,20 +29,20 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.all(32.0),
+              padding: const EdgeInsets.all(12.0),
               child: Column(children: <Widget>[
                 Text(
                   'Consejos',
                   style: TextStyle(
                       fontFamily: 'Avenir',
-                      fontSize: 44,
-                      color: const Color(0xff215466),
+                      fontSize: 40,
+                      color: Color(0xFF8215466),
                       fontWeight: FontWeight.w900),
                 )
               ]),
             ),
             Container(
-              height: 500,
+              height: MediaQuery.of(context).size.height- 4 * 64,
               padding: const EdgeInsets.only(left: 32),
               child: Swiper(
                 itemCount: consejos.length,
@@ -56,63 +57,71 @@ class HomePage extends StatelessWidget {
                   alignment: Alignment.bottomCenter,
                 ),
                 itemBuilder: (context, index) {
-                  return Stack(children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        SizedBox(
-                          height: 70,
-                        ),
-                        Card(
-                          elevation: 8,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(32)),
-                          color: Color(0xffE5f5f3),
-                          child: Padding(
-                            padding: const EdgeInsets.all(32.0),
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  SizedBox(height: 100),
-                                  Text(
-                                    consejos[index].name,
-                                    style: TextStyle(
-                                        fontFamily: 'Avenir',
-                                        fontSize: 40,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w900),
-                                        textAlign: TextAlign.center,
-                                  ),
-                                  SizedBox(height: 32),
-                                  Text(
-                                    consejos[index].description,
-                                    style: TextStyle(
-                                        fontFamily: 'Avenir',
-                                        fontSize: 20,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w500),
-                                  ),
-                                  SizedBox(height: 50),
-                                  /* Row(
-                                    children: <Widget>[
-                                      Text(
-                                        'Mas información',
-                                        style: TextStyle(
-                                            fontFamily: 'Avenir',
-                                            fontSize: 16,
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.w500),
-                                        textAlign: TextAlign.right,
-                                      ),
-                                      Icon(Icons.arrow_forward)
-                                    ],
-                                  ) */
-                                ]),
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(context, PageRouteBuilder(pageBuilder: (context,a,b)=>DetailPage(consejo: consejos[index])));
+                    },
+                    child: Stack(children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 40,
                           ),
-                        )
-                      ],
-                    ),
-                    /* Image.asset(consejos[index].iconImage) */
-                  ]);
+                          Card(
+                            elevation: 8,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(32)),
+                            color: Color(0xffE5f5f3),
+                            child: Padding(
+                              padding: const EdgeInsets.all(32.0),
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    SizedBox(height: 100),
+                                    Text(
+                                      consejos[index].name,
+                                      style: TextStyle(
+                                          fontFamily: 'Avenir',
+                                          fontSize: 24,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w900),
+                                          textAlign: TextAlign.center,
+                                    ),
+                                    SizedBox(height: 32),
+                                    Text(
+                                      consejos[index].description,
+                                      maxLines: 4,
+                                      style: TextStyle(
+                                          fontFamily: 'Avenir',
+                                          fontSize: 18,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    SizedBox(height: 50),
+                                    /* Row(
+                                      children: <Widget>[
+                                        Text(
+                                          'Mas información',
+                                          style: TextStyle(
+                                              fontFamily: 'Avenir',
+                                              fontSize: 16,
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.w500),
+                                          textAlign: TextAlign.right,
+                                        ),
+                                        Icon(Icons.arrow_forward)
+                                      ],
+                                    ) */
+                                  ]),
+                            ),
+                          )
+                        ],
+                      ),
+                      Hero(
+                        tag: consejos[index].position,
+                        child: Image.asset(consejos[index].iconImage))
+                    ]),
+                  );
                 },
               ),
             )
